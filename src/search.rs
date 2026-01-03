10565158dbf4c024bcb643642a0e944fed18b511
Quinten Kock <quinten@quinten.space>
2026-01-03 03:15:51 +0100

    put TT code in separate file

--- src/search.rs
+++ src/search.rs
@@ -14,6 +14,10 @@ use shakmaty::{
     zobrist::{Zobrist64, ZobristHash},
 };
 
+mod tt;
+
+use tt::*;
+
 pub struct NodeCount {
     pub nodes: AtomicU64,
     pub leaves: AtomicU64,
@@ -34,10 +38,6 @@ enum MoveOrderKey {
     TTMove(i16),
     Capture(i16, i16), // victim value, aggressor value
     Quiet(i16),        // development value
-}
-
-fn move_match_tt(m: &Move, tte: &TTEntry) -> bool {
-    (m.from().unwrap() as u8 == tte.from) && (m.to() as u8 == tte.to)
 }
 
 fn move_key(pos: &Chess, tte: Option<TTEntry>, m: &Move) -> MoveOrderKey {
@@ -72,67 +72,6 @@ fn move_key(pos: &Chess, tte: Option<TTEntry>, m: &Move) -> MoveOrderKey {
         let devel = value_new - value_old;
         MoveOrderKey::Quiet(-devel)
     }
-}
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-enum ScoreType {
-    Exact = 0,
-    LowerBound = 1,
-    UpperBound = 2,
-}
-
-// Transposition table
-// TT-Entry bitmap:
-// 24 bits: high part of zobrist key (useful up to 2^40 entries, after that some bits become redundant with index; maybe use buckets at some point?)
-// 8 bits: search depth
-// 16 bits: score
-// 6 bits: from square
-// 6 bits: to square
-// 2 bits: score type
-// 2 bits: free!
-
-#[derive(Clone, Copy)]
-struct TTEntry {
-    // basic best-move info
-    from: u8,
-    to: u8,
-
-    // TODO: value info etc
-    value: i16,
-    depth: u8,
-    score_type: ScoreType,
-}
-
-fn get_tt(tt: &Vec<AtomicU64>, moves: &[Move], key: u64) -> Option<TTEntry> {
-    let index = (key % tt.len() as u64) as usize;
-    let entry = tt[index].load(std::sync::atomic::Ordering::Relaxed);
-    if entry >> 40 == (key >> 40) {
-        let depth = ((entry >> 32) & 0xFF) as u8;
-        let value = ((entry >> 16) & 0xFFFF) as i16;
-        let from = ((entry >> 10) & 0x3F) as u8;
-        let to = ((entry >> 4) & 0x3F) as u8;
-        let score_type = match (entry >> 2) & 0x3 {
-            0 => ScoreType::Exact,
-            1 => ScoreType::LowerBound,
-            2 => ScoreType::UpperBound,
-            _ => unreachable!(),
-        };
-        let entry = TTEntry { from, to, value, depth, score_type };
-        if moves.iter().any(|m| move_match_tt(m, &entry)) { Some(entry) } else { None }
-    } else {
-        None
-    }
-}
-
-fn write_tt(tt: &Vec<AtomicU64>, key: u64, data: TTEntry) {
-    let index = (key % tt.len() as u64) as usize;
-    let entry = (key & 0xFFFFFF0000000000) // basically << 40 but keeping the high part
-        | ((data.depth as u64) << 32)
-        | ((data.value.cast_unsigned() as u64) << 16)
-        | ((data.from as u64) << 10)
-        | ((data.to as u64) << 4)
-        | ((data.score_type as u64) << 2);
-    tt[index].store(entry, std::sync::atomic::Ordering::Relaxed);
 }
 
 // Actual search implementation
