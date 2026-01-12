df9bb8ff4cbae12a42f9350592c60c9ccecc55ae
Quinten Kock <quintenkock@gmail.com>
2026-01-12 02:40:10 +0100

    add extra bench position

--- src/bench.rs
+++ src/bench.rs
@@ -2,9 +2,10 @@ use shakmaty::Chess;
 
 use crate::{search::search, time};
 
-const POSITIONS: [(&str, isize); 3] = [
+const POSITIONS: [(&str, isize); 4] = [
     ("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 8),
     ("r1bq1rk1/4ppbp/p1pp1np1/1P2n3/2B1PB2/2NP1N1P/1PP2PP1/R2QR1K1 b - - 0 11", 7),
+    ("2r3r1/3R2pk/p1p1PB2/1pR2P2/2p1PK2/P1P5/8/5b2 w - - 9 19", 7),
     ("8/k7/3p4/p2P1p2/P2P1P2/8/8/K7 w - ", 30),
 ];
 
